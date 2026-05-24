d114acd90b36552f311de211577113b0021ceb8b
Lucas Bittencourt <lucasbt@gmail.com>
2026-05-23 23:19:16 -0300

    Fix backup_file function call in DNF configuration

--- modules/00-system.sh
+++ modules/00-system.sh
@@ -125,7 +125,7 @@ _configure_dnf() {
   step "Configuring DNF for better performance"
 
   local dnf_conf="/etc/dnf/dnf.conf"
-  run_as_root backup_file "$dnf_conf"
+  backup_file "$dnf_conf"
 
   local max_p="${DNF_MAX_PARALLEL:-10}"
   local keepcache="${DNF_KEEPCACHE:-false}"
